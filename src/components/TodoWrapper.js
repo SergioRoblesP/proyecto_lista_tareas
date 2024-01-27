8c18cc0d76bf50c186edc34413875b15ec226e43
Kavch <Kevinchoez1234@gmail.com>
2024-01-27 13:34:07 -0500

    Se agg la funcionalidad de marcar una tarea como realizada

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -8,14 +8,19 @@ export const TodoWrapper = () => {
     const [todos, setTodos] = useState([])
     const addTodo = todo =>{
         setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
-        console.log(todos)
+        console.log(todos) 
+    }
+    const toggleComplete =id => {
+        setTodos(todos.map(todo => todo.id === id ? {...todo, completed : !todo.completed}: todo))
     }
 
     return (
         <div className='TodoWrapper'>
+            <h1> Agregar Tarea</h1>
             <TodoForm addTodo={addTodo} />
             {todos.map((todo, index) => (
-                <Todo task={todo} key={index}></Todo>
+                <Todo task={todo} key={index} toggleComplete={toggleComplete}></Todo>
+
 
             ))} 
         </div>

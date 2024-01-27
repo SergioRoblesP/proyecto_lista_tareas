ca7c0787a573774e5a7d82be337356d46e9e0808
SERGIODEFJAM <SERGIODEFJAM@DESKTOP-5MUH9LS>
2024-01-27 11:37:17 -0500

    Se genera la base de la aplicación, añadiendo estilos, imágenes de la universidad y la base del formulario. Queda pendiente añadirle las funciones de agregar, marcar como completada, eliminar y editar.

---
+++ src/components/TodoForm.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+export const TodoForm = () => {
+    const [value, setValue] = useState("")
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        console.log(value)
+    }
+    return (
+        <form className='TodoForm' onSubmit={handleSubmit}>
+            <input type='text' className='todo-input'
+            placeholder='Ingresa la tarea a realizar' 
+            onChange={(e) => setValue(e.target.value)}></input>
+            <button type='submit' className='todo-btn'>Agregar tarea</button>
+        </form>
+    )
+}
---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import { TodoForm } from './TodoForm'
+
+export const TodoWrapper = () => {
+    const [todos, setTodos] = useState([])
+    return (
+        <div className='TodoWrapper'>
+            <TodoForm></TodoForm>
+        </div>
+    )
+}
